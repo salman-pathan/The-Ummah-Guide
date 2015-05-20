8a30cf058bab8da4d8a2bdc6c28a643fc527259c
Salman Khan <salmanpathan30@gmail.com>
2015-05-20 09:39:40 -0700

    Added Free and Paid build flavors

---
+++ app/src/free/java/com/codiodes/theummahguide/Constants.java
@@ -0,0 +1,8 @@
+package com.codiodes.theummahguide;
+
+/**
+ * Created by Salman on 5/20/2015.
+ */
+public class Constants {
+    public static final AppType APP_TYPE = AppType.FREE;
+}
---
+++ app/src/main/java/com/codiodes/theummahguide/AppType.java
@@ -0,0 +1,8 @@
+package com.codiodes.theummahguide;
+
+/**
+ * Created by Salman on 5/20/2015.
+ */
+public enum AppType {
+    FREE, PAID
+}
